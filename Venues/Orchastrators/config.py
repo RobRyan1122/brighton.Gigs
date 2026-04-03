085255462f94879ba419f2cd5e5404e4d8504f7a
RobRyan1122 <114590371+RobRyan1122@users.noreply.github.com>
2026-04-03 20:24:42 +0100

    Fix emailer path to be relative

--- Venues/Orchastrators/config.py
+++ Venues/Orchastrators/config.py
@@ -4,4 +4,4 @@ EMAILADDRESS = "robautomation57@gmail.com"
 PASSWORD = "lyztqxzaxryrnegu"
 RECIPIENT_EMAIL = "robautomation57@gmail.com"
 
-FOLDER_PATH = Path(r"C:\Users\PC\OneDrive\Documents\BrightonLocalGigs\output_daily")
+FOLDER_PATH = Path(__file__).resolve().parent.parent.parent / "output_daily"
